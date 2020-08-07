cf7d204eaa870768b4c55ed95206ae9bccae29d9
Tyler <bisig1892@gmail.com>
2020-08-06 21:31:31 -0700

    Added Recipe Class

---
+++ TeamSourceControlDemo/Recipe.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamSourceControlDemo
+{
+    class Recipe
+    {
+        /// <summary>
+        /// The Id of the recipe in the database
+        /// </summary>
+        public int RecipeId { get; set; }
+        /// <summary>
+        /// The name of the recipe
+        /// </summary>
+        public string Title { get; set; }
+        /// <summary>
+        /// The total amount of time from start to completion of the recipe
+        /// </summary>
+        public int TotalTime { get; set; }
+        /// <summary>
+        /// The amount to time to prep the ingredients for the recipe
+        /// </summary>
+        public int PrepTime { get; set; }
+        /// <summary>
+        /// The amount of servings the recipe makes
+        /// </summary>
+        public int Servings { get; set; }
+        /// <summary>
+        /// A list of ingredients needed for the recipe
+        /// </summary>
+        public string Ingredients { get; set; }
+        /// <summary>
+        /// Instructions on how to make the food
+        /// </summary>
+        public string Instructions { get; set; }
+    }
+}
