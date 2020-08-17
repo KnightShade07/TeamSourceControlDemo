56dc144516adbccb352f516caff8ca7b161c4968
Tyler <bisig1892@gmail.com>
2020-08-16 19:51:58 -0700

    Created new landing form with buttons to new forms for CRUD functionality. Started RecipeDB.

---
+++ TeamSourceControlDemo/EditDeletForm.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace TeamSourceControlDemo
+{
+    partial class EditDeletForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.IngredientsTxt = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.ServingsTxt = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.RecipeTitleTxt = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.EditBtn = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.InstructionsTxt = new System.Windows.Forms.TextBox();
+            this.PrepTimeTxt = new System.Windows.Forms.TextBox();
+            this.TotalTimeTxt = new System.Windows.Forms.TextBox();
+            this.DeleteBtn = new System.Windows.Forms.Button();
+            this.ExitBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // IngredientsTxt
+            // 
+            this.IngredientsTxt.Location = new System.Drawing.Point(246, 106);
+            this.IngredientsTxt.Multiline = true;
+            this.IngredientsTxt.Name = "IngredientsTxt";
+            this.IngredientsTxt.Size = new System.Drawing.Size(100, 154);
+            this.IngredientsTxt.TabIndex = 26;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(243, 76);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(59, 13);
+            this.label7.TabIndex = 25;
+            this.label7.Text = "Ingredients";
+            // 
+            // ServingsTxt
+            // 
+            this.ServingsTxt.Location = new System.Drawing.Point(113, 191);
+            this.ServingsTxt.Name = "ServingsTxt";
+            this.ServingsTxt.Size = new System.Drawing.Size(100, 20);
+            this.ServingsTxt.TabIndex = 24;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 119);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 13);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "Total Time";
+            // 
+            // RecipeTitleTxt
+            // 
+            this.RecipeTitleTxt.Location = new System.Drawing.Point(113, 81);
+            this.RecipeTitleTxt.Name = "RecipeTitleTxt";
+            this.RecipeTitleTxt.Size = new System.Drawing.Size(100, 20);
+            this.RecipeTitleTxt.TabIndex = 18;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 84);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 13);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Recipe Name";
+            // 
+            // EditBtn
+            // 
+            this.EditBtn.Location = new System.Drawing.Point(12, 291);
+            this.EditBtn.Name = "EditBtn";
+            this.EditBtn.Size = new System.Drawing.Size(123, 47);
+            this.EditBtn.TabIndex = 29;
+            this.EditBtn.Text = "Save Changes";
+            this.EditBtn.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 29);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Edit/Delete Recipe";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(357, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 27;
+            this.label3.Text = "Instructions";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "PrepTime";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 194);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "Servings Made";
+            // 
+            // InstructionsTxt
+            // 
+            this.InstructionsTxt.Location = new System.Drawing.Point(360, 106);
+            this.InstructionsTxt.Multiline = true;
+            this.InstructionsTxt.Name = "InstructionsTxt";
+            this.InstructionsTxt.Size = new System.Drawing.Size(191, 101);
+            this.InstructionsTxt.TabIndex = 28;
+            // 
+            // PrepTimeTxt
+            // 
+            this.PrepTimeTxt.Location = new System.Drawing.Point(113, 151);
+            this.PrepTimeTxt.Name = "PrepTimeTxt";
+            this.PrepTimeTxt.Size = new System.Drawing.Size(100, 20);
+            this.PrepTimeTxt.TabIndex = 22;
+            // 
+            // TotalTimeTxt
+            // 
+            this.TotalTimeTxt.Location = new System.Drawing.Point(113, 116);
+            this.TotalTimeTxt.Name = "TotalTimeTxt";
+            this.TotalTimeTxt.Size = new System.Drawing.Size(100, 20);
+            this.TotalTimeTxt.TabIndex = 20;
+            // 
+            // DeleteBtn
+            // 
+            this.DeleteBtn.Location = new System.Drawing.Point(141, 291);
+            this.DeleteBtn.Name = "DeleteBtn";
+            this.DeleteBtn.Size = new System.Drawing.Size(123, 47);
+            this.DeleteBtn.TabIndex = 30;
+            this.DeleteBtn.Text = "Delete Recipe";
+            this.DeleteBtn.UseVisualStyleBackColor = true;
+            // 
+            // ExitBtn
+            // 
+            this.ExitBtn.Location = new System.Drawing.Point(270, 291);
+            this.ExitBtn.Name = "ExitBtn";
+            this.ExitBtn.Size = new System.Drawing.Size(123, 47);
+            this.ExitBtn.TabIndex = 31;
+            this.ExitBtn.Text = "Exit";
+            this.ExitBtn.UseVisualStyleBackColor = true;
+            // 
+            // EditDeletForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(568, 383);
+            this.Controls.Add(this.ExitBtn);
+            this.Controls.Add(this.DeleteBtn);
+            this.Controls.Add(this.IngredientsTxt);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.ServingsTxt);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.RecipeTitleTxt);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.EditBtn);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.InstructionsTxt);
+            this.Controls.Add(this.PrepTimeTxt);
+            this.Controls.Add(this.TotalTimeTxt);
+            this.Name = "EditDeletForm";
+            this.Text = "EditDeletForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox IngredientsTxt;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox ServingsTxt;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox RecipeTitleTxt;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button EditBtn;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox InstructionsTxt;
+        private System.Windows.Forms.TextBox PrepTimeTxt;
+        private System.Windows.Forms.TextBox TotalTimeTxt;
+        private System.Windows.Forms.Button DeleteBtn;
+        private System.Windows.Forms.Button ExitBtn;
+    }
+}
