bc7a86eb48b546acb6dd35cabb8092dc18e7cd02
Tyler <bisig1892@gmail.com>
2020-08-18 10:50:02 -0700

    added code to populate textboxes of recipe that was passed from the RecipeMain form

--- TeamSourceControlDemo/EditDeleteForm.cs
+++ TeamSourceControlDemo/EditDeleteForm.cs
@@ -12,9 +12,33 @@ namespace TeamSourceControlDemo
 {
     public partial class EditDeleteForm : Form
     {
+        public int CurrRecipeId { get; set; }
         public EditDeleteForm()
         {
             InitializeComponent();
+        }
+
+        public EditDeleteForm(int id)
+        {
+            InitializeComponent();
+            CurrRecipeId = id;
+        }
+
+        private void EditDeleteForm_Load(object sender, EventArgs e)
+        {
+            CurrRecipeId = RecipeMain.currRecipeId;
+            Recipe currRecipe = RecipeDb.GetRecipe(CurrRecipeId);
+            PopulateCurrentRecipe(currRecipe);
+        }
+
+        private void PopulateCurrentRecipe(Recipe r)
+        {
+            RecipeTitleTxt.Text = r.Title;
+            TotalTimeTxt.Text = r.TotalTime.ToString();
+            PrepTimeTxt.Text = r.PrepTime.ToString();
+            ServingsTxt.Text = r.Servings.ToString();
+            IngredientsTxt.Text = r.Ingredients.ToString();
+            InstructionsTxt.Text = r.Instructions.ToString();
         }
     }
 }
